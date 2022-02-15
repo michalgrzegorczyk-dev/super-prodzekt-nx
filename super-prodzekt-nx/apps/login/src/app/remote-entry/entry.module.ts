39207b7df2b6ebc21c7047d1e56588c5cedad6a5
Filip Hartman <Filip_Hartman@epam.com>
2022-02-15 22:01:02 +0100

    mfe setup

---
+++ super-prodzekt-nx/apps/login/src/app/remote-entry/entry.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { RemoteEntryComponent } from './entry.component';
+import {FormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [RemoteEntryComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: RemoteEntryComponent,
+      },
+    ]),
+  ],
+  providers: [],
+})
+export class RemoteEntryModule {}
---
+++ super-prodzekt-nx/apps/nx-shell/src/app/app.component.ts
@@ -0,0 +1,52 @@
+import {Component, OnInit} from '@angular/core';
+import {UserService} from "@super-prodzekt-nx/shared/data-access-user";
+import {Router} from "@angular/router";
+import {distinctUntilChanged} from "rxjs";
+
+@Component({
+  selector: 'super-prodzekt-nx-root',
+  template: `
+    <header>Super header super prodzektu
+      <div *ngIf="isLoggedIn$ | async">User zalogowany</div>
+    </header>
+    <div *ngIf="isLoggedIn$ | async; else signIn">
+      You are authenticated so you can see this content.
+    </div>
+    <ng-template #signIn>
+      <router-outlet></router-outlet>
+    </ng-template>
+    <footer>Super footer super prodzektu</footer>
+  `,
+  styles: [`
+    header {
+      width: 100%;
+      height: 5rem;
+      background-color: red;
+      color: white
+    }
+
+    footer {
+      width: 100%;
+      height: 3rem;
+      background-color: blue;
+      color: white
+    }
+  `]
+})
+export class AppComponent implements OnInit {
+  isLoggedIn$ = this.userService.isUserLoggedIn$;
+
+  constructor(private readonly userService: UserService, private readonly router: Router) {
+  }
+
+  ngOnInit() {
+    this.isLoggedIn$.pipe(distinctUntilChanged())
+      .subscribe((loggedIn: boolean) => {
+        if (!loggedIn) {
+          this.router.navigateByUrl('login');
+        } else {
+          this.router.navigateByUrl('');
+        }
+      })
+  }
+}
